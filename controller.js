1d885c6b732f9804c6c27599f8c522d34b47e303
Jeff Pickelman <pickelman@gmail.com>
2014-02-05 11:03:34 -0800

    Minor refactor of initialize_board -> initialize_game

--- controller.js
+++ controller.js
@@ -1,15 +1,15 @@
 
 function TicTacToeCtrl($scope) {
   $scope.board_size = 3;
-  $scope.current_move = 'A';
   
-  $scope.initialize_board = function () {
+  $scope.initialize_game = function () {
+    $scope.current_move = 'A';
     $scope.board = [];
     for (var i = 0; i < $scope.board_size; i++) {
       $scope.board[i] = new Array($scope.board_size);
     }
   };
-  $scope.initialize_board();
+  $scope.initialize_game();
   
   $scope.mark_pane = function (parent_index, index) {
     console.log('marking pane (' + parent_index + ',' + index + ')!');
