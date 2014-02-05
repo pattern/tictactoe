21a5fcd80bf73cbd994f6866fda7e1d083ba47d3
Jeff Pickelman <pickelman@gmail.com>
2014-02-05 10:50:45 -0800

    Add ".clearfix" class definition

--- controller.js
+++ controller.js
@@ -1,4 +1,17 @@
 
 function TicTacToeCtrl($scope) {
-  console.log('Controller loaded!');
+  $scope.board_size = 3;
+  $scope.current_move = 'A';
+  
+  $scope.initialize_board = function () {
+    $scope.board = [];
+    for (var i = 0; i < $scope.board_size; i++) {
+      $scope.board[i] = new Array($scope.board_size);
+    }
+  };
+  $scope.initialize_board();
+  
+  $scope.mark_pane = function (parent_index, index) {
+    console.log('marking pane (' + parent_index + ',' + index + ')!');
+  };
 }
